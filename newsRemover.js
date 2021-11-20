21f20beda318b0f2c48cba54eaaeb38033f9a3be
Bon <10662057+BonMutti@users.noreply.github.com>
2021-11-20 11:20:36 -0500

    Update newsRemover.js
    updated with a function delcaration and call

--- newsRemover.js
+++ newsRemover.js
@@ -1,7 +1,9 @@
-$(document).ready(function () {
+const remNews = function () {
   setTimeout(function () {
     $(
       ".h676nmdw.bx45vsiw.dawyy4b1.jrc8bbd0.o5zgeu5y.to382e16.buofh1pr"
     ).remove();
-  }, 500);
-});
+  }, 300);
+};
+
+$(document).ready(remNews);
